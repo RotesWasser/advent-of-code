c3f1344c174b78e3d458511dfb52b5fe06395abe
RotesWasser <hallo@roteswasser.com>
2021-12-02 00:53:43 +0100

    Solved day 1-2

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,9 +1,19 @@
-use std::{env, iter};
+extern crate clap;
+
+use clap::{App, Arg};
 
 fn main() {
-    let cmd_args: Vec<String> = env::args().collect();
+    let matches = App::new("Advent of Code Day 1")
+                    .arg(Arg::with_name("sliding-window")
+                        .help("Use sliding window as required by the second challenge.")
+                        .long("sliding-window"))
+                    .arg(Arg::with_name("INPUT")
+                        .help("Sonar readings file to parse.")
+                        .required(true)
+                        .index(1))
+                    .get_matches();
 
-    let depth_readings_contents = std::fs::read_to_string(&cmd_args[1])
+    let depth_readings_contents = std::fs::read_to_string(&matches.value_of("INPUT").unwrap())
         .expect("Failed to open the readings file");
 
     let readings_extracted = depth_readings_contents
@@ -11,7 +21,13 @@ fn main() {
         .filter(|reading| !reading.is_empty())
         .map(|reading| reading.parse::<i32>().unwrap());
     
-    let depth_increases = calculate_depth_increases(readings_extracted);
+    let sums: Vec<i32> = if matches.is_present("sliding-window") {
+        calculate_triplet_sums(readings_extracted).collect()
+    } else {
+        readings_extracted.collect()
+    };
+    
+    let depth_increases = calculate_depth_increases(sums);
 
     println!("Number of depth increases: {}", depth_increases);
 }
@@ -21,13 +37,27 @@ fn calculate_depth_increases<'a, I>(values: I) -> usize
 where
     I: IntoIterator<Item = i32> + Clone
 {
-    let previous = values.clone().into_iter().chain(iter::repeat(0));
-    let mut current = values.into_iter();
-    current.next();
+    let previous = values.clone().into_iter();
+    let current = values.into_iter().skip(1);
 
     let depth_increases = previous.zip(current).filter(|(prev, current)| prev < current).count();
 
     return depth_increases;
+}
+
+fn calculate_triplet_sums<'a, I>(values: I) -> impl Iterator<Item = i32>
+where
+    I: IntoIterator<Item = i32> + Clone 
+{
+    let first = values.clone().into_iter();
+    let second = values.clone().into_iter().skip(1);
+    let third = values.clone().into_iter().skip(2);
+
+    let res = first.zip(second).zip(third).map(|((a, b), c)| {
+        a + b + c
+    });
+
+    return res;
 }
 
 #[test]
@@ -35,4 +65,22 @@ fn test_simple() {
     let test_data = vec![0, 1, 0, 2];
 
     assert_eq!(calculate_depth_increases(test_data), 2);
+}
+
+#[test]
+fn test_triplet_sums_simple() {
+    let test_data = vec![199, 200, 208];
+
+    let computed: Vec<i32> = calculate_triplet_sums(test_data).collect();
+
+    assert_eq!(computed, vec![607]);
+}
+
+#[test]
+fn test_triplet_sums_complex() {
+    let test_data = vec![199, 200, 208, 210, 200];
+
+    let computed: Vec<i32> = calculate_triplet_sums(test_data).collect();
+
+    assert_eq!(computed, vec![607, 618, 618]);
 }
